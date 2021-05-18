90e04d8f7c016696c925093a1e8b34371c8f4652
akuleshov <akuleshov@5prosoftware.com>
2021-05-18 13:35:30 +0300

    AA-38_registration_initial_screen(fix init viewBinding)

--- app/src/main/java/nl/appt/auth/AuthFragment.kt
+++ app/src/main/java/nl/appt/auth/AuthFragment.kt
@@ -1,26 +1,38 @@
 package nl.appt.auth
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import nl.appt.R
 import nl.appt.databinding.FragmentAuthBinding
 import nl.appt.widgets.ToolbarFragment
 
 class AuthFragment : ToolbarFragment() {
 
-    private lateinit var binding: FragmentAuthBinding
-
     override fun getTitle() = getString(R.string.tab_training)
 
     override fun getLayoutId() = R.layout.fragment_auth
+
+    private var _binding: FragmentAuthBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUi()
     }
 
-    fun initUi() {
-        binding = FragmentAuthBinding.inflate(layoutInflater)
+    private fun initUi() {
         binding.createAccountBtn.setOnClickListener {
             //Open Create Account Screen
         }
@@ -30,5 +42,10 @@ class AuthFragment : ToolbarFragment() {
         binding.forgotPasswordBtn.setOnClickListener {
             //Open Reset Password Screen
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
