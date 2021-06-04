a9dc209b1b669c0ec8a4837f0e52b97d1a3d74c6
akuleshov <akuleshov@5prosoftware.com>
2021-06-04 18:36:43 +0300

    feature/AA-52

--- app/src/main/java/nl/appt/MainActivity.kt
+++ app/src/main/java/nl/appt/MainActivity.kt
@@ -1,13 +1,18 @@
 package nl.appt
 
+import android.view.Gravity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import nl.appt.accessibility.Accessibility
 import nl.appt.accessibility.isTalkBackEnabled
 import nl.appt.helpers.Events
+import nl.appt.helpers.Preferences
+import nl.appt.helpers.UserConst
 import nl.appt.tabs.home.HomeFragment
 import nl.appt.tabs.knowledge.KnowledgeFragment
 import nl.appt.tabs.more.MoreFragment
@@ -47,6 +52,7 @@ class MainActivity : BaseActivity() {
 
     override fun onViewCreated() {
         this.title = ""
+        checkUserMailVerification()
         events.property(Events.Property.talkback, Accessibility.isTalkBackEnabled(this))
 
         // Tab adapter
@@ -82,6 +88,33 @@ class MainActivity : BaseActivity() {
             tabs.contains(item.itemId)
         }
         navigationView.selectedItemId = tabs[0]
+    }
+
+    private fun checkUserMailVerification() {
+        if (Preferences.getString(UserConst.USER_VERIFIED_KEY) == "0") {
+            showSnackbar()
+        }
+    }
+
+    private fun showSnackbar() {
+        val snackbar = Snackbar.make(
+            coordinatorLayout,
+            getString(R.string.user_not_verified_allert),
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setAction(getString(R.string.allert_action_title)) {}
+            .setActionTextColor(getColor(R.color.row))
+            .setTextColor(getColor(R.color.row))
+            .setBackgroundTint(getColor(R.color.red))
+            .setAnimationMode(Snackbar.ANIMATION_MODE_FADE)
+        val styledAttributes =
+            theme.obtainStyledAttributes(intArrayOf(android.R.attr.actionBarSize))
+        val view = snackbar.view
+        val params = view.layoutParams as CoordinatorLayout.LayoutParams
+        params.gravity = Gravity.TOP
+        params.topMargin = styledAttributes.getDimension(0, 0f).toInt()
+        view.layoutParams = params
+        snackbar.show()
     }
 
     fun setPagerItem(itemNumber: Int) {
