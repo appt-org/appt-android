90c18b57044b4d20f0bec65ad9e7f9d29823bcf2
akuleshov <akuleshov@5prosoftware.com>
2021-06-08 17:02:53 +0300

    feedback/AA-83

--- app/src/main/java/nl/appt/tabs/more/ProfileActivity.kt
+++ app/src/main/java/nl/appt/tabs/more/ProfileActivity.kt
@@ -2,6 +2,7 @@ package nl.appt.tabs.more
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import nl.appt.R
 import nl.appt.auth.AuthActivity
@@ -50,7 +51,7 @@ class ProfileActivity : ToolbarActivity() {
             viewModel.changePassword(Preferences.getString(UserConst.USER_EMAIL_KEY))
         }
         binding.deleteAccount.setOnClickListener {
-            viewModel.deleteUser(Preferences.getInt(UserConst.USER_ID_KEY))
+            deleteAccount()
         }
     }
 
@@ -80,6 +81,19 @@ class ProfileActivity : ToolbarActivity() {
                 }
             }
         }
+    }
+
+    private fun deleteAccount() {
+        AlertDialog.Builder(this, R.style.Dialog)
+            .setTitle(getString(R.string.profile_delete_account))
+            .setMessage(getString(R.string.delete_account_message))
+            .setPositiveButton(getString(R.string.delete_account_positive_button)) { _, _ ->
+                viewModel.deleteUser(Preferences.getInt(UserConst.USER_ID_KEY))
+            }
+            .setNegativeButton(getString(R.string.delete_account_negative_button)) { _, _ ->
+                //Ignore
+            }
+            .create().show()
     }
 
     private fun toAuthActivity() {
