4b309027f54b063613642cb2b58c5deeb22bcc24
Jan Jaap de Groot <info@janjaap.com>
2021-08-06 16:20:16 +0200

    Add Android R accessibility service gestures

--- app/src/main/java/nl/appt/model/AccessibilityGesture.kt
+++ app/src/main/java/nl/appt/model/AccessibilityGesture.kt
@@ -1,6 +1,8 @@
 package nl.appt.model
 
 import android.accessibilityservice.AccessibilityService
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 /**
  * Created by Jan Jaap de Groot on 15/10/2020
@@ -8,29 +10,95 @@ import android.accessibilityservice.AccessibilityService
  */
 enum class AccessibilityGesture(val gestureId: Int) {
 
-    // UP
+    // ONE FINGER SWIPE UP
     SWIPE_UP(AccessibilityService.GESTURE_SWIPE_UP),
     SWIPE_UP_AND_RIGHT(AccessibilityService.GESTURE_SWIPE_UP_AND_RIGHT),
     SWIPE_UP_AND_DOWN(AccessibilityService.GESTURE_SWIPE_UP_AND_DOWN),
     SWIPE_UP_AND_LEFT(AccessibilityService.GESTURE_SWIPE_UP_AND_LEFT),
 
-    // RIGHT
+    // ONE FINGER SWIPE RIGHT
     SWIPE_RIGHT(AccessibilityService.GESTURE_SWIPE_RIGHT),
     SWIPE_RIGHT_AND_UP(AccessibilityService.GESTURE_SWIPE_RIGHT_AND_UP),
     SWIPE_RIGHT_AND_DOWN(AccessibilityService.GESTURE_SWIPE_RIGHT_AND_DOWN),
     SWIPE_RIGHT_AND_LEFT(AccessibilityService.GESTURE_SWIPE_RIGHT_AND_LEFT),
 
-    // DOWN
+    // ONE FINGER SWIPE DOWN
     SWIPE_DOWN(AccessibilityService.GESTURE_SWIPE_DOWN),
     SWIPE_DOWN_AND_UP(AccessibilityService.GESTURE_SWIPE_DOWN_AND_UP),
     SWIPE_DOWN_AND_RIGHT(AccessibilityService.GESTURE_SWIPE_DOWN_AND_RIGHT),
     SWIPE_DOWN_AND_LEFT(AccessibilityService.GESTURE_SWIPE_DOWN_AND_LEFT),
 
-    // LEFT
+    // ONE FINGER SWIPE LEFT
     SWIPE_LEFT(AccessibilityService.GESTURE_SWIPE_LEFT),
     SWIPE_LEFT_AND_UP(AccessibilityService.GESTURE_SWIPE_LEFT_AND_UP),
     SWIPE_LEFT_AND_RIGHT(AccessibilityService.GESTURE_SWIPE_LEFT_AND_RIGHT),
-    SWIPE_LEFT_AND_DOWN(AccessibilityService.GESTURE_SWIPE_LEFT_AND_DOWN);
+    SWIPE_LEFT_AND_DOWN(AccessibilityService.GESTURE_SWIPE_LEFT_AND_DOWN),
+
+    // ONE FINGER TAP
+    @RequiresApi(Build.VERSION_CODES.R)
+    ONE_FINGER_DOUBLE_TAP(AccessibilityService.GESTURE_DOUBLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    ONE_FINGER_DOUBLE_TAP_HOLD(AccessibilityService.GESTURE_DOUBLE_TAP_AND_HOLD),
+
+    // TWO FINGER TAP
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_SINGLE_TAP(AccessibilityService.GESTURE_2_FINGER_SINGLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_DOUBLE_TAP(AccessibilityService.GESTURE_2_FINGER_DOUBLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_DOUBLE_TAP_HOLD(AccessibilityService.GESTURE_2_FINGER_DOUBLE_TAP_AND_HOLD),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_TRIPLE_TAP(AccessibilityService.GESTURE_2_FINGER_TRIPLE_TAP),
+
+    // TWO FINGER SWIPE
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_SWIPE_UP(AccessibilityService.GESTURE_2_FINGER_SWIPE_UP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_SWIPE_RIGHT(AccessibilityService.GESTURE_2_FINGER_SWIPE_RIGHT),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_SWIPE_DOWN(AccessibilityService.GESTURE_2_FINGER_SWIPE_DOWN),
+    @RequiresApi(Build.VERSION_CODES.R)
+    TWO_FINGER_SWIPE_LEFT(AccessibilityService.GESTURE_2_FINGER_SWIPE_LEFT),
+
+    // THREE FINGER TAP
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_SINGLE_TAP(AccessibilityService.GESTURE_3_FINGER_SINGLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_DOUBLE_TAP(AccessibilityService.GESTURE_3_FINGER_DOUBLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_DOUBLE_TAP_HOLD(AccessibilityService.GESTURE_3_FINGER_DOUBLE_TAP_AND_HOLD),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_TRIPLE_TAP(AccessibilityService.GESTURE_3_FINGER_TRIPLE_TAP),
+
+    // THREE FINGER SWIPE
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_SWIPE_UP(AccessibilityService.GESTURE_3_FINGER_SWIPE_UP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_SWIPE_RIGHT(AccessibilityService.GESTURE_3_FINGER_SWIPE_RIGHT),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_SWIPE_DOWN(AccessibilityService.GESTURE_3_FINGER_SWIPE_DOWN),
+    @RequiresApi(Build.VERSION_CODES.R)
+    THREE_FINGER_SWIPE_LEFT(AccessibilityService.GESTURE_3_FINGER_SWIPE_LEFT),
+
+    // FOUR FINGER TAP
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_SINGLE_TAP(AccessibilityService.GESTURE_4_FINGER_SINGLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_DOUBLE_TAP(AccessibilityService.GESTURE_4_FINGER_DOUBLE_TAP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_DOUBLE_TAP_HOLD(AccessibilityService.GESTURE_4_FINGER_DOUBLE_TAP_AND_HOLD),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_TRIPLE_TAP(AccessibilityService.GESTURE_4_FINGER_TRIPLE_TAP),
+
+    // FOUR FINGER SWIPE
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_SWIPE_UP(AccessibilityService.GESTURE_4_FINGER_SWIPE_UP),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_SWIPE_RIGHT(AccessibilityService.GESTURE_4_FINGER_SWIPE_RIGHT),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_SWIPE_DOWN(AccessibilityService.GESTURE_4_FINGER_SWIPE_DOWN),
+    @RequiresApi(Build.VERSION_CODES.R)
+    FOUR_FINGER_SWIPE_LEFT(AccessibilityService.GESTURE_4_FINGER_SWIPE_LEFT);
 
     val directions: Array<Direction>
         get() {
@@ -54,6 +122,8 @@ enum class AccessibilityGesture(val gestureId: Int) {
                 SWIPE_LEFT_AND_UP -> arrayOf(Direction.LEFT, Direction.UP)
                 SWIPE_LEFT_AND_RIGHT -> arrayOf(Direction.LEFT, Direction.RIGHT)
                 SWIPE_LEFT_AND_DOWN -> arrayOf(Direction.LEFT, Direction.DOWN)
+
+                else -> arrayOf()
             }
 
             array.forEach { direction ->
